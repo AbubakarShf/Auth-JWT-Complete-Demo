2ca008c92c0e014a83fb0040b386e811db1ba78d
AbubakarShf <ItsAbubakarShf@gmail.com>
2021-12-30 16:08:02 +0500

    Cookies-done

--- auth_app/views.py
+++ auth_app/views.py
@@ -8,21 +8,39 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.exceptions import AuthenticationFailed
 # Create your views here.
-
+import jwt, datetime
+import requests
+from django.shortcuts import reverse
+from django.shortcuts import render, redirect, HttpResponseRedirect
 class Register(APIView):
     RegisterSerializer_Class=RegisterSerializer
+    def get(self,request):
+        return render(request, 'register.html')
     def post(self,request,format=None):
         serializer=self.RegisterSerializer_Class(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"status":status.HTTP_200_OK,"Message":"Registered Successfully!"})
+            msg={
+                'msg':"Registered Successfully"
+            }
+            #return Response({"status":status.HTTP_200_OK,"Message":"Registered Successfully!"})
+            return render(request, 'login.html',msg)
         else:
             return Response({"Message":serializer.errors,"status":status.HTTP_400_BAD_REQUEST})
 class Login(APIView):
+    def get(self,request):
+        if 'logged_in' in request.COOKIES and 'Access_Token' in request.COOKIES:
+            context = {
+                'Access_Token': request.COOKIES['Access_Token'],
+                'logged_in': request.COOKIES.get('logged_in'),
+            }
+            return render(request, 'abc.html', context)
+        else:
+            return render(request, 'login.html')
     def post(self,request,format=None):
-        email = request.data['email']
-        password = request.data['password']
-
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        print(email,password)
         user = User.objects.filter(email=email).first()
 
         if user is None:
@@ -31,14 +49,32 @@ class Login(APIView):
         if not user.check_password(password):
             raise AuthenticationFailed('Incorrect password!')
 
-        refresh = RefreshToken.for_user(user)
-        return Response({"status":status.HTTP_200_OK,"RefreshToken":str(refresh),"AccessToken":str(refresh.access_token)})
 
+        refresh = RefreshToken.for_user(user)
+        print("Access Token")
+        print(str(refresh.access_token))
+        response=render(request,'students.html')
+        response.set_cookie('Access_Token',str(refresh.access_token))
+        response.set_cookie('logged_in', True)
+        return response
+
+        # refresh = RefreshToken.for_user(user)
+        # print("Refresh Token")
+        # print(str(refresh))
+        # print("Access Token")
+        # print(str(refresh.access_token))
+        # data={
+        #     'accesstoken':str(refresh.access_token)
+        # }
 
 class StudentData(APIView):
     StudentSerializer_Class=StudentSerializer
-    permission_classes=[IsAuthenticated,]
+    #permission_classes=[IsAuthenticated]
     def get(self,request,format=None):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
         DataObj=Student.objects.all()
         serializer=self.StudentSerializer_Class(DataObj,many=True)
         serializerData=serializer.data
@@ -56,9 +92,13 @@ class StudentData(APIView):
 class Logout(APIView):
     def post(self,request):
         try:
-            refersh_token=request.data.get('refresh_token')
-            tokenObj = RefreshToken(refersh_token)
-            tokenObj.blacklist()
-            return Response({"status":status.HTTP_200_OK,"Message":"LOGOUT"})
+
+            response = HttpResponseRedirect(reverse('login'))
+
+            # deleting cookies
+            response.delete_cookie('Access_Token')
+            response.delete_cookie('logged_in')
+
+            return response
         except:
             return Response({"status":status.HTTP_400_BAD_REQUEST})
--- auth_project/urls.py
+++ auth_project/urls.py
@@ -4,5 +4,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("auth_app.urls")),
+    path('', include("auth_app.urls")),
 ]
